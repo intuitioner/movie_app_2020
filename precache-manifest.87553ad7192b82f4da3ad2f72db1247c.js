a823f783c5f8701221a5ba8b2029c8a11a2d9708
Intuitioner <intuitioner1@gmail.com>
2020-04-05 16:50:46 +0900

    Updates

---
+++ precache-manifest.87553ad7192b82f4da3ad2f72db1247c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d311719eaae78c8d0f398e2f5cf6dbf",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "bb02351230401f00332e",
+    "url": "/movie_app_2020/static/css/main.53b0a393.chunk.css"
+  },
+  {
+    "revision": "978fd30c2dffb856c8af",
+    "url": "/movie_app_2020/static/js/2.f3c4edb5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/movie_app_2020/static/js/2.f3c4edb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb02351230401f00332e",
+    "url": "/movie_app_2020/static/js/main.d8f9ec00.chunk.js"
+  },
+  {
+    "revision": "01aae0007e1cfd873da8",
+    "url": "/movie_app_2020/static/js/runtime-main.482f7179.js"
+  }
+]);
